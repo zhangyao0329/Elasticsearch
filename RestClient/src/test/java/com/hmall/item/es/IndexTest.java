09908572249b7cf9ddec01b90b2e142f93e11b29
ZhangYao <1870476411@qq.com>
2025-12-05 00:48:58 +0800

    操作Index（类Table）

--- RestClient/src/test/java/com/hmall/item/es/IndexTest.java
+++ RestClient/src/test/java/com/hmall/item/es/IndexTest.java
@@ -7,10 +7,8 @@ import org.elasticsearch.client.RestHighLevelClient;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
-
 
 public class IndexTest {
 
